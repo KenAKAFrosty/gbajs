f97a2389b2b71d9db8d5ee008dd81fe89a48cc60
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-04-03 00:43:43 -0400

    twitch integration now live, added r/l bumper

--- js/twitchIntegration.js
+++ js/twitchIntegration.js
@@ -1,1 +1,29 @@
-console.log('test')
+const streamerName = "KenAKAFrosty"
+
+
+
+async function startTwitchIntegration(){ 
+    ComfyJS.onReward = (user, reward, cost, message, extra) => {
+        handleRewardRedemption(user, reward, cost, message, extra)
+    }
+
+    ComfyJS.Init(streamerName, oAuthToken)
+}
+
+function handleRewardRedemption(user, reward, cost, message, extra) {
+    console.log(user, reward, cost, message, extra)
+    const rewardNameToButton = { 
+        'Press Up 👆':'Up',
+        'Press Left 👈':'Left',
+        'Press Right 👉':'Right',
+        'Press Down 👇':'Down',
+        'Press A':'A',
+        'Press B':'B',
+        'Press Select':'Select',
+        'Press Start':'Start',
+        'Press Left Bumper':'BumperL',
+        'Press Right Bumper':'BumperR'
+    }
+
+    pressButton(rewardNameToButton[reward])
+}
